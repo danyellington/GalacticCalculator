7ae36c281193471b1d975ab3fca480b5ecf35393
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-09 11:45:49 -0800

    work on getting user bday and returning in seconds

--- js/calculator.js
+++ js/calculator.js
@@ -4,9 +4,10 @@ export class BirthDate{
   }
   // function getAge(dateString){
   //   let today = new Date();
-  //     const birthDate = new Date(dateString);
-  //     const age = today.getFullYear() - birthDate.getFullYear();
-  //     const m = today.getMonth() - birthDate.getMonth();
+  //     var birthDate = new Date(dateString);
+  //     var age = today.getFullYear() - birthDate.getFullYear();
+  //     var m = today.getMonth() - birthDate.getMonth();
+  //     var d = today.getDay() - birthDate.getDay();
   //     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
   //         age--;
   //     }
@@ -17,4 +18,19 @@ export class BirthDate{
   var birthday = +new Date(dateString);
   return ~~((Date.now() - birthday) / (31557600000));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
