e6476fa43005bcfcd7c2175d567276d19dad5b4d
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-09 11:10:36 -0800

    add some html to display results.

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
   $('#signup').submit(function(event){
     event.preventDefault();
-    var name = $('#name').val();
-    var number = $('#number').val();
-    var preference = $('#preference').val();
-    var description = $('#description').val();
-    var match = $('#name', '#number', '#preference', '#description');
-    $('#signup').hide();
-    $('#results').show(genderArray);
+    var birthday = $('#birthday').val();
+    $('#results').show(birthday);
   });
 });
--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,20 @@
 export class BirthDate{
   constructor(birthDate) {
-    this.birthDate = birthDate
+    this.birthDate = birthDate;
   }
-  function getAge(dateString){
-    let today = new Date();
-      const birthDate = new Date(dateString);
-      const age = today.getFullYear() - birthDate.getFullYear();
-      const m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-          age--;
-      }
-      return age;
-  }
-};
+  // function getAge(dateString){
+  //   let today = new Date();
+  //     const birthDate = new Date(dateString);
+  //     const age = today.getFullYear() - birthDate.getFullYear();
+  //     const m = today.getMonth() - birthDate.getMonth();
+  //     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+  //         age--;
+  //     }
+  //     return age;
+  // }
+
+  function(dateString) {
+  var birthday = +new Date(dateString);
+  return ~~((Date.now() - birthday) / (31557600000));
+}
+}
