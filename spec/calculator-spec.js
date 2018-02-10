c35049ddb954918d74d5357e87d727d46d1f722f
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-09 16:38:10 -0800

    as good as it gets for today.

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -6,12 +6,7 @@ import {Age} from "./../js/calculator.js";
 //     expect(birthday).toEqual(birthday);
 //   });
 
-  // it('should add match to gender array', function(){
-  //   var match = new Match("Ted","Male","5035152234","Female","Hit it and quit it");
-  //   var matchUpdate = match.genderArray();
-  //   console.log("test" +matchUpdate);
-  //   expect(matchUpdate).toEqual(["Ted"]);
-  // });
+
 // describe('getAge', function(){
 //   it('should return birthDate in seconds', function(){
 //   var age = new age(2);
